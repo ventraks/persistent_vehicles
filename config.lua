a69274e70ca9d3c8d34c07762ffc0130ec262815
ventraks <83806248+ventraks@users.noreply.github.com>
2025-04-01 11:40:47 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+-- Set to true if you want debug messages in the console
+Config.Debug = true
+
+-- Which player identifier type to use (e.g., 'license', 'steam', 'discord', 'fivem')
+-- Ensure this identifier is unique and persistent for your players. 'license' is common.
+Config.PlayerIdentifier = 'license'
+
+-- How often (in milliseconds) the client should check if the player exited a vehicle
+Config.CheckInterval = 1000 -- 1 second
+
+-- Add any other configuration options here
